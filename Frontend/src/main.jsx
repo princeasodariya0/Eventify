f0a6b8a55fe79795c849f6de21aeab88e363d356
Prince Asodariya <princeasodariya001@gmail.com>
2026-06-13 22:21:35 +0530

    Fix AuthProvider wrapping

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import axios from "axios";
 
+import { AuthProvider } from "./context/AuthContext";
 import App from "./App.jsx";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -14,12 +15,14 @@ axios.defaults.withCredentials = true;
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
       <ToastContainer
         position="top-left"
         autoClose={3000}
         hideProgressBar={false}
       />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
